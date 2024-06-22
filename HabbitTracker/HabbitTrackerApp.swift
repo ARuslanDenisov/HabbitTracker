82b5d88af8a2abcf89e64431d09b21b0e0726069
ARuslanDenisov <angess47@gmail.com>
2024-06-22 14:06:11 +0700

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Руслан on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
